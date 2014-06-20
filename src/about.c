7d431faf2d589d2da21c286d62762d6ec02e7f42
Ilan Pegoraro <iemoreirap@gmail.com>
2014-06-20 14:20:51 +0100

    Fixes issues with the about dialog.
    The about dialog was only shown the first time, failing to be shown after being closed

--- src/about.c
+++ src/about.c
@@ -24,68 +24,40 @@ void about_display()
 {
 	//gtk_widget_show(GTK_WIDGET(dlg_about));
 	//gtk_show_about_dialog(GTK_ABOUT_DIALOG(dlg_about), NULL);
-	gtk_widget_show(GTK_WIDGET(dlg_about));
-}
-
-void about_init(GtkBuilder *builder)
-{
+	//gtk_widget_show(GTK_WIDGET(dlg_about));
 	GdkPixbuf *logo;
 	GError *error = NULL;
-
-	dlg_about = GTK_WINDOW(gtk_about_dialog_new());
 	char buffer[200];
-	gchar *authors[3] = {
+	const gchar *authors[3] = {
 		"Pedro Ávila Lopes <paol1976@yahoo.com>",
 		"Ilan Moreira Pegoraro <iemoreirap@gmail.com>",
 		NULL
 	};
-	gchar *translators[17] = {
-		"Rafael Bermúdez (es) <rafaelber@yahoo.com.mx>",
-		"Christian Bjelle (sv) <christian@bjelle.se>",
-		"Dovydas <laisve (lt)@gmail.com>",
-		"Daniel van (nl) Eeden <daniel_e@dds.nl>",
-		"Rafal Glazar (pl) <rafal_glazar@o2.pl>",
-		"Dmytro Goykolov (ua) <goykolov@pisem.net>",
-		"Jeremie Knuesel (fr) <jeremie.knuesel@epfl.ch>",
-		"Oleksandr Korneta (ua) <atenrok@ua.fm>",
-		"Vítězslav Kotrla (cs) <vitko@post.cz>",
-		"Pedro Ávila (pt) Lopes <paol1976@yahoo.com>",
-		"Pavel Maryanov (ru) <acid_jack@ukr.net>",
-		"Christopher Orr (en_GB) <chris@protactin.co.uk>",
-		"Rostislav Raykov (bg) <zbrox@i-space.org>",
-		"Emilio Scalise (it) <emisca@rocketmail.com>",
-		"Lucas Mazzardo (pt_BR) Veloso <lmveloso@gmail.com>",
-		"Jan Wenzel (de) <scripter01@gmx.net>",
-		NULL
-	};
-	
-	/* set the title */
-	gtk_window_set_title(dlg_about, _("About Audio Tag Tool"));
 
-	gtk_about_dialog_set_program_name(GTK_ABOUT_DIALOG(dlg_about), PACKAGE_NAME);
-	gtk_about_dialog_set_version(GTK_ABOUT_DIALOG(dlg_about), PACKAGE_VERSION);
-	gtk_about_dialog_set_license_type(GTK_ABOUT_DIALOG(dlg_about), GTK_LICENSE_GPL_2_0);
-	gtk_about_dialog_set_website(GTK_ABOUT_DIALOG(dlg_about), "http://pwp.netcabo.pt/paol/tagtool/");
-	gtk_about_dialog_set_authors(GTK_ABOUT_DIALOG(dlg_about), (const gchar **)authors);
-	gtk_about_dialog_set_license(GTK_ABOUT_DIALOG(dlg_about),
-					_("This software is made available under the GNU General Public Licence." \
-					"See the file COPYING for the full license terms."));
-	
-	
-	/* set the team of translators */
-	//gtk_about_dialog_set_translator_credits(GTK_ABOUT_DIALOG(dlg_about), "Translators");
-	gtk_about_dialog_add_credit_section (GTK_ABOUT_DIALOG(dlg_about),
-                                     _("Translated by:"),
-                                     (const gchar**)translators);
+	const gchar* copyright = "Copyright \xc2\xa9 2008 Pedro Lopes.\n" \
+                             "Copyright \xc2\xa9 2013 Ilan Pegoraro.\n";
 
-	logo = gdk_pixbuf_new_from_file(DATADIR"/TagTool.png", &error);
-	if(logo != NULL) 
-		gtk_about_dialog_set_logo(GTK_ABOUT_DIALOG(dlg_about), logo);
-	else {
-		printf("Error: unable to logo: %s", error->message);
-		g_clear_error(&error);
-	}
-
+	const gchar *translators = 
+		"Rafael Bermúdez (es) <rafaelber@yahoo.com.mx>\n" \
+		"Christian Bjelle (sv) <christian@bjelle.se>\n" \
+		"Dovydas <laisve (lt)@gmail.com>\n" \
+		"Daniel van (nl) Eeden <daniel_e@dds.nl>\n" \
+		"Rafal Glazar (pl) <rafal_glazar@o2.pl>\n" \
+		"Dmytro Goykolov (ua) <goykolov@pisem.net>\n" \
+		"Jeremie Knuesel (fr) <jeremie.knuesel@epfl.ch>\n" \
+		"Oleksandr Korneta (ua) <atenrok@ua.fm>\n" \
+		"Vítězslav Kotrla (cs) <vitko@post.cz>\n" \
+		"Pedro Ávila (pt) Lopes <paol1976@yahoo.com>\n" \
+		"Pavel Maryanov (ru) <acid_jack@ukr.net>\n" \
+		"Christopher Orr (en_GB) <chris@protactin.co.uk>\n" \
+		"Rostislav Raykov (bg) <zbrox@i-space.org>\n" \
+		"Emilio Scalise (it) <emisca@rocketmail.com>\n" \
+		"Lucas Mazzardo Veloso (pt_BR) <lmveloso@gmail.com>\n" \
+		"Jan Wenzel (de) <scripter01@gmx.net>\n";
+	
+	gchar *license = _("This software is made available under the GNU General Public Licence." \
+					"See the file COPYING for the full license terms.");
+	
 	// /* set the supported audio formats */
 	strcpy(buffer, _("Easily manage the tags of your music files.\n Supported audio formats: "));
 
@@ -100,5 +72,24 @@ void about_init(GtkBuilder *builder)
 	strcat(buffer, "Ogg Vorbis");
 #endif
 
-	gtk_about_dialog_set_comments(GTK_ABOUT_DIALOG(dlg_about), buffer);
+	logo = gdk_pixbuf_new_from_file(DATADIR"/TagTool.png", &error);
+	if(!logo) {
+		printf("Error: unable to logo: %s", error->message);
+		g_clear_error(&error);
+	}
+	gtk_show_about_dialog(NULL, "authors", (const gchar **)authors, 
+			"program-name", PACKAGE_NAME,
+			"version", PACKAGE_VERSION,
+			"logo", logo,
+			"website", "https://github.com/impegoraro/tagtool/wiki",
+			"comments", buffer,
+			"license-type", GTK_LICENSE_GPL_2_0,
+			"license", license,
+			"translator-credits", translators, 
+			"copyright", copyright,
+			NULL);
+}
+
+void about_init(GtkBuilder *builder)
+{	
 }
